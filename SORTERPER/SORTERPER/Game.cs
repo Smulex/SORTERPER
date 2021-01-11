1dddd38db6b64eb8c0919fa04efea150157cbb8f
Christian Bøgh <chri45i4@zbc.dk>
2021-01-11 11:38:46 +0100

    .

--- SORTERPER/SORTERPER/Game.cs
+++ SORTERPER/SORTERPER/Game.cs
@@ -11,6 +11,27 @@ namespace SORTERPER
         public List<Card> Cards { get; set; }
         public List<Player> Players { get; set; }
 
+        public void CheckForPairManually(Player player, Card card1, Card card2)
+        {
+            if (card1.Number == card2.Number)
+            {
+                List<Card> itemsToRemove = new List<Card>();
+                itemsToRemove.Add(player.Hand[j]);
+                itemsToRemove.Add(player.Hand[i]);
+
+
+                foreach (Card value in itemsToRemove)
+                {
+                    player.Hand = player.Hand.Where(x => x != value).ToList();
+                }
+
+                if (!player.Hand.Any())
+                {
+                    player.Done = true;
+                }
+            }
+        }
+
         public void CheckForPair(Player player)
         {
             for (int i = 0; i < player.Hand.Count; i++)
@@ -40,6 +61,26 @@ namespace SORTERPER
                 }
             }
         }
+
+        public void AddPlayer(string name)
+        {
+            //Adds one player
+            Players.Add(new Player(name));
+        }
+
+        public void AddCards()
+        {
+            //Adding all the cards in a standard deck of 25 cards.
+            for (int i = 1; i <= 12; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Cards.Add(new Card(i));
+                }
+            }
+            Cards.Add(new Card(0));
+        }
+
         public void ShuffleCards()
         {
             this.Cards = this.Cards.OrderBy(a => Guid.NewGuid()).ToList();
--- SORTERPER/SORTERPER/Program.cs
+++ SORTERPER/SORTERPER/Program.cs
@@ -10,17 +10,6 @@ namespace SORTERPER
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-
-            //Add all the cards in a standard deck of 25 card deck.
-            for (int i = 1; i <= 12; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    game.Cards.Add(new Card(i));
-                }
-            }
-            game.Cards.Add(new Card(0));
 
             Console.ReadKey();
         }
